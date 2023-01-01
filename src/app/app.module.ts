31424c6a7deb2036cc24257d4066a6ca05fcda0f
Rayncor <sowa332@gmail.com>
2022-12-31 16:27:22 -0800

    added services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
 
+//singleton pattern is a single instance of a given object in memory passed to several different constructors or components 
+//dont forget dependency injection step 
 export class AppModule { }
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,12 +1,27 @@
 import { Component } from "@angular/core";
+import { CoursesService } from "./courses.service";
 
 //need to import component decorator for typescript to export to angular 
 
 @Component({
     selector: 'courses', 
-    template: '<h1>Hello</h1>'
+    template: `<h1>{{getTitle()}}</h1>
+               <ul>
+                <li *ngFor="let course of courses">{{course}}</li>
+    `
 })
 
 export class CoursesComponent {
+    title = "List of courses";
+    courses;
 
+    constructor(service: CoursesService) {
+        this.courses =  service.getCourses();
+
+        //need to instruct angular to inject the dependency of this argument into this constructor 
+    }
+
+    getTitle() {
+        return this.title;
+    }
 }
